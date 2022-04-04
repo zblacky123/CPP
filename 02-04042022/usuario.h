50ca0ff855e5f932c1081c59b7ed671afb586e97
Orlando Alves <orlandoabneto@gmail.com>
2022-04-04 11:08:18 -0300

    Criacao da classe Usuario

---
+++ 02-04042022/usuario.h
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+
+using std::cout;
+using std::cin;
+using std::string;
+
+class Usuario{
+
+    public: 
+        string nome;
+        string senha;
+        string email;
+        string nivel;
+
+
+};
+
